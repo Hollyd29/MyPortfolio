76f6e634acb125375ec5824c19990a7cb835b687
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-07-14 23:52:22 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import HeaderComp from "./component/headercomp";
+
+function App() {
+  return (
+    <div>
+      <HeaderComp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Headercomp.tsx
@@ -0,0 +1,29 @@
+function HeaderComp() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#b0e0feff",
+      }}
+    >
+      <div
+        style={{
+          border: "2px solid red",
+        }}
+      >
+        <h3
+          style={{
+            marginLeft: 10,
+            padding: -10,
+            color: "#0062FF",
+            textAlign: "center",
+            fontSize: 30,
+            lineHeight: 1,
+          }}
+        >
+          PORTFOLIO
+        </h3>
+      </div>
+    </div>
+  );
+}
+export default HeaderComp;
