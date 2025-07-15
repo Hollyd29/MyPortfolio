6e87a665aa03ce2c9cadc2a452c845d07b1ce4e9
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-07-15 17:21:09 +0100

    profile componernt almost done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import HeaderComp from "./component/headercomp";
+import Profilecomp from "./component/Profilecomp";
 
 function App() {
   return (
     <div>
       <HeaderComp />
+      <Profilecomp />
     </div>
   );
 }
--- src/component/Profilecomp.tsx
+++ src/component/Profilecomp.tsx
@@ -1,1 +1,64 @@
+function Profilecomp() {
+  return (
+    <div>
+      <h1 style={{ marginTop: 70, textAlign: "center" }}>Oladimeji E.O.</h1>
+      <p style={{ textAlign: "center", fontSize: 18 }}>
+        I am a{" "}
+        <span
+          style={{
+            fontWeight: 600,
+            color: "#0062FF",
+            textTransform: "capitalize",
+          }}
+        >
+          professional mobile app developer
+        </span>{" "}
+        with strong expertise in{" "}
+        <span
+          style={{
+            fontWeight: 600,
+            color: "#0062FF",
+            textTransform: "capitalize",
+          }}
+        >
+          React Native and TypeScript
+        </span>{" "}
+        .{" "}
+        <p>
+          {" "}
+          I specialize in {""}
+          <span
+            style={{
+              fontWeight: 600,
+              color: "#0062FF",
+              textTransform: "capitalize",
+            }}
+          >
+            building responsive, high-performance mobile applications for both
+            iOS and Android
+          </span>
+          . With a solid background in{" "}
+          <span
+            style={{
+              fontWeight: 600,
+              color: "#0062FF",
+              textTransform: "capitalize",
+            }}
+          >
+            API integration
+          </span>{" "}
+          and a passion for clean, scalable code, I deliver seamless user
+          experiences that meet client goals. Whether working on business tools
+          or consumer apps, I bring precision, speed, and reliability to every
+          project. I'm always learning and committed to building modern,
+          impactful solutions.
+        </p>
+      </p>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+}
 
+export default Profilecomp;
