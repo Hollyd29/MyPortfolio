d1292c8c6bdd9a7971ca9f72edb88fc8da35868b
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-07-17 23:36:19 +0100

    contact me updated

--- src/component/ContactMecomp.tsx
+++ src/component/ContactMecomp.tsx
@@ -1,28 +1,26 @@
 function ContactMeComp() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1 style={{ textAlign: "center" }}>Reach Me Here!</h1>
+    <div className="contactCon">
+      <h1 className="contactHeading">Reach Me Here!</h1>
       <p style={{ marginBottom: 50 }}>
         I am currently open to joining a new team of creative and curious
         developers, reach me below. Also, if you have proposals, questions or
         you just want to say 'hi', you can reach out too
       </p>
-      <div style={{ opacity: 0.6 }}>
+      <div className="contacts">
         <p>Liverpool UK</p>
       </div>
-      <div style={{ opacity: 0.6 }}>
+      <div className="contacts">
         <p>+44 744 301 0680</p>
       </div>
-      <div style={{ opacity: 0.6 }}>
+      <div className="contacts">
         <p>olapejuoladimeji30@gmail.com</p>
       </div>
-      <div style={{ textAlign: "center", marginTop: 100 }}>
+      <div className="letWork">
         <h3>Want me to work for you or with you?</h3>
         <p>Let's connect! 👍</p>
       </div>
-      <p style={{ opacity: 0.6, marginTop: 50, textAlign: "center" }}>
-        Copyright - Oladimeji E.O 2025
-      </p>
+      <p className="copyright">Copyright - Oladimeji E.O 2025</p>
     </div>
   );
 }
