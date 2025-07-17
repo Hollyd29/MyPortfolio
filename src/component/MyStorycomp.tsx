a815bbc838257845bd5498bd94666ad0a4241c4b
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-07-17 22:42:33 +0100

    my story page updated

--- src/component/MyStorycomp.tsx
+++ src/component/MyStorycomp.tsx
@@ -1,56 +1,22 @@
 function MyStoryComp() {
   return (
-    <div style={{ padding: 20, backgroundColor: "#b0e0feff" }}>
+    <div className="storyCon">
       <h1>
         My <br /> Story
       </h1>
 
       <p>
-        My name is{" "}
-        <span
-          style={{
-            fontWeight: 800,
-            color: "#0062FF",
-          }}
-        >
-          Oladimeji
-        </span>{" "}
-        , a curious and passionate{" "}
-        <span
-          style={{
-            fontWeight: 800,
-            color: "#0062FF",
-          }}
-        >
-          Software Developer
-        </span>{" "}
-        who enjoys converting imaginations and ideas into real life products and
-        building solutions that run on the internet. I have been coding
-        consistently in{" "}
-        <span
-          style={{
-            fontWeight: 800,
-            color: "#0062FF",
-          }}
-        >
+        My name is <span className="storyColorText">Oladimeji</span> , a curious
+        and passionate{" "}
+        <span className="storyColorText">Software Developer</span> who enjoys
+        converting imaginations and ideas into real life products and building
+        solutions that run on the internet. I have been coding consistently in{" "}
+        <span className="storyColorText">
           JavaScript, TypeScript, and technologies
         </span>{" "}
-        around them for over{" "}
-        <span
-          style={{
-            fontWeight: 800,
-            color: "#0062FF",
-          }}
-        >
-          3 years
-        </span>{" "}
-        . I have used these technologies to build fully blown applications like{" "}
-        <span
-          style={{
-            fontWeight: 800,
-            color: "#0062FF",
-          }}
-        >
+        around them for over <span className="storyColorText">3 years</span> . I
+        have used these technologies to build fully blown applications like{" "}
+        <span className="storyColorText">
           E-commerce Application, Url-customization Application and I will be
           working Booking Application like Uber
         </span>
@@ -67,28 +33,16 @@ function MyStoryComp() {
         the project section of my protfolio.
       </p>
       <p>
-        <span
-          style={{
-            fontWeight: 800,
-            fontSize: 18,
-          }}
-        >
+        <span className="boldText">
           What I work with: <br /> Languages:
         </span>{" "}
         HTML, CSS, JavaScript, TypeScript, Node.js (Open to learn more as it is
         required by the job)
       </p>
       <p>
-        <span
-          style={{
-            fontWeight: 800,
-            fontSize: 18,
-          }}
-        >
-          Technologies:
-        </span>{" "}
-        React.js, React Native, Styled-Components, TailwindCSS, Git, Jest and
-        React Testing Tool (Open to learn more as it is required by the job)
+        <span className="boldText">Technologies:</span> React.js, React Native,
+        Styled-Components, TailwindCSS, Git, Jest and React Testing Tool (Open
+        to learn more as it is required by the job)
       </p>
     </div>
   );
