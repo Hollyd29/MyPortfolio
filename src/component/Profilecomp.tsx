b20c3f126139ddd1f355a8d309e158ac7fc1db01
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-07-17 21:34:58 +0100

    profile comp updated

--- src/component/Profilecomp.tsx
+++ src/component/Profilecomp.tsx
@@ -2,73 +2,30 @@ import image from "../assets/image.jpeg";
 
 function Profilecomp() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1 style={{ marginTop: 120, textAlign: "center" }}>Oladimeji .E.O.</h1>
-      <p style={{ textAlign: "center", fontSize: 18 }}>
+    <div className="profileCon">
+      <h1 className="name">Oladimeji .E.O.</h1>
+      <p className="aboutMe">
         I am a{" "}
-        <span
-          style={{
-            fontWeight: 800,
-            color: "#0062FF",
-            textTransform: "capitalize",
-          }}
-        >
-          professional mobile app developer
-        </span>{" "}
+        <span className="colorText">professional mobile app developer</span>{" "}
         with strong expertise in{" "}
-        <span
-          style={{
-            fontWeight: 800,
-            color: "#0062FF",
-            textTransform: "capitalize",
-          }}
-        >
-          React Native and TypeScript
-        </span>{" "}
-        .{" "}
+        <span className="colorText">React Native and TypeScript</span> .{" "}
         <p>
           {" "}
           I specialize in {""}
-          <span
-            style={{
-              fontWeight: 800,
-              color: "#0062FF",
-              textTransform: "capitalize",
-            }}
-          >
+          <span className="colorText">
             building responsive, high-performance mobile applications for both
             iOS and Android
           </span>
           . With a solid background in{" "}
-          <span
-            style={{
-              fontWeight: 600,
-              color: "#0062FF",
-              textTransform: "capitalize",
-            }}
-          >
-            API integration
-          </span>{" "}
-          and a passion for clean, scalable code, I deliver seamless user
-          experiences that meet client goals. Whether working on business tools
-          or consumer apps, I bring precision, speed, and reliability to every
-          project. I'm always learning and committed to building modern,
-          impactful solutions.
+          <span className="colorText">API integration</span> and a passion for
+          clean, scalable code, I deliver seamless user experiences that meet
+          client goals. Whether working on business tools or consumer apps, I
+          bring precision, speed, and reliability to every project. I'm always
+          learning and committed to building modern, impactful solutions.
         </p>
       </p>
-      <div
-        style={{
-          width: "50%",
-          height: 350,
-          marginBlock: 50,
-          marginInline: "auto",
-        }}
-      >
-        <img
-          style={{ width: "100%", height: "100%", borderRadius: 10 }}
-          src={image}
-          alt=""
-        />
+      <div className="imgCon">
+        <img className="img" src={image} alt="" />
       </div>
       <div style={{ textAlign: "center" }}>
         <h2>A Developer with curious mind.</h2>
