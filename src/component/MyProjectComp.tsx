ccb82dabef620bcba6e7daccc33750509776c9a0
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-07-20 02:24:13 +0100

    comfiable updated

--- src/component/MyProjectComp.tsx
+++ src/component/MyProjectComp.tsx
@@ -3,22 +3,26 @@ import comfiable from "../assets/projects/confiable-Home.mp4";
 
 function MyProjectComp() {
   return (
-    <div style={{ backgroundColor: "black", color: "#ffffff" }}>
+    <div style={{ backgroundColor: "black", color: "#ffffff", padding: 20 }}>
       <div>
         <h1>MY PROJECT</h1>
         <p>Each one is unique, check them out.</p>
       </div>
       <div>
         <h2>Comfiable Home</h2>
-        <video
-          className="comfiable-Vid"
-          src={comfiable}
-          controls
-          autoPlay
-          loop
-          muted
-          playsInline
-        ></video>
+        <div
+          style={{ display: "flex", alignItems: "center", textAlign: "center" }}
+        >
+          <video
+            className="comfiable-Vid"
+            src={comfiable}
+            controls
+            autoPlay
+            loop
+            muted
+            playsInline
+          ></video>
+        </div>
       </div>
     </div>
   );
