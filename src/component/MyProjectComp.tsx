684b030aaa30e840f8ed231cb3a100d58d6dfd7d
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-07-20 01:12:30 +0100

    My project continution

--- src/component/MyProjectComp.tsx
+++ src/component/MyProjectComp.tsx
@@ -1,5 +1,16 @@
 function MyProjectComp() {
-  return <div>My Project Comp</div>;
+  return (
+    <div>
+      <div>
+        <h1>MY PROJECT</h1>
+        <p>Each one is unique, check them out.</p>
+      </div>
+      <div>
+        <h2>Comfiable Home</h2>
+        <video src=""></video>
+      </div>
+    </div>
+  );
 }
 
 export default MyProjectComp;
