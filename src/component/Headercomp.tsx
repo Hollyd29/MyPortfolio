42bfddea36f7ffb45ff134d9cb1ca3ec3c104220
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-07-21 05:24:59 +0100

    menu drop down started

--- src/component/Headercomp.tsx
+++ src/component/Headercomp.tsx
@@ -1,16 +1,32 @@
 import { FaBars } from "react-icons/fa";
 import "./portfolio.css";
+import { useState } from "react";
 
 function HeaderComp() {
+  const [menu, setMenu] = useState<boolean>(false);
+
   return (
-    <div className="headerCon">
-      <div>
-        <h3 className="logo">PORTFOLIO</h3>
+    <>
+      <div className="headerCon">
+        <div>
+          <h3 className="logo">PORTFOLIO</h3>
+        </div>
+        <div className="iconCon">
+          <FaBars className="headerIcon" />
+        </div>
       </div>
-      <div className="iconCon">
-        <FaBars className="headerIcon" />
-      </div>
-    </div>
+      {menu && (
+        <div
+          style={{
+            height: "100%",
+            width: "100%",
+            backgroundColor: "black",
+            position: "absolute",
+            top: 0,
+          }}
+        ></div>
+      )}
+    </>
   );
 }
 export default HeaderComp;
