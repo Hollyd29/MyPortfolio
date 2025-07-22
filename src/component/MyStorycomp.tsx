b51804c8e7f26382d3625f3e5cf7a91854e0a07d
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-07-22 19:31:48 +0100

    done with my story media quary

--- src/component/MyStorycomp.tsx
+++ src/component/MyStorycomp.tsx
@@ -1,49 +1,51 @@
 function MyStoryComp() {
   return (
     <div className="storyCon">
-      <h1>
-        My <br /> Story
-      </h1>
+      <div className="storyInnerCon">
+        <h1>
+          My <br /> Story
+        </h1>
 
-      <p>
-        My name is <span className="storyColorText">Oladimeji</span> , a curious
-        and passionate{" "}
-        <span className="storyColorText">Software Developer</span> who enjoys
-        converting imaginations and ideas into real life products and building
-        solutions that run on the internet. I have been coding consistently in{" "}
-        <span className="storyColorText">
-          JavaScript, TypeScript, and technologies
-        </span>{" "}
-        around them for over <span className="storyColorText">3 years</span> . I
-        have used these technologies to build fully blown applications like{" "}
-        <span className="storyColorText">
-          E-commerce Application, Url-customization Application and I will be
-          working Booking Application like Uber
-        </span>
-        .
-      </p>
-      <p>
-        One thing I like to do is completing projects. I strive hard to complete
-        projects and even improve them over time. I believe 2 to 3 complete
-        projects are better than 10 incomplete projects
-      </p>
-      <p>
-        To speak less of myself with words , but my work experiences(There is a
-        saying that it's not how far but how well), I would like you to explore
-        the project section of my protfolio.
-      </p>
-      <p>
-        <span className="boldText">
-          What I work with: <br /> Languages:
-        </span>{" "}
-        HTML, CSS, JavaScript, TypeScript, Node.js (Open to learn more as it is
-        required by the job)
-      </p>
-      <p>
-        <span className="boldText">Technologies:</span> React.js, React Native,
-        Styled-Components, TailwindCSS, Git, Jest and React Testing Tool (Open
-        to learn more as it is required by the job)
-      </p>
+        <p>
+          My name is <span className="storyColorText">Oladimeji</span> , a
+          curious and passionate{" "}
+          <span className="storyColorText">Software Developer</span> who enjoys
+          converting imaginations and ideas into real life products and building
+          solutions that run on the internet. I have been coding consistently in{" "}
+          <span className="storyColorText">
+            JavaScript, TypeScript, and technologies
+          </span>{" "}
+          around them for over <span className="storyColorText">3 years</span> .
+          I have used these technologies to build fully blown applications like{" "}
+          <span className="storyColorText">
+            E-commerce Application, Url-customization Application and I will be
+            working Booking Application like Uber
+          </span>
+          .
+        </p>
+        <p>
+          One thing I like to do is completing projects. I strive hard to
+          complete projects and even improve them over time. I believe 2 to 3
+          complete projects are better than 10 incomplete projects
+        </p>
+        <p>
+          To speak less of myself with words , but my work experiences(There is
+          a saying that it's not how far but how well), I would like you to
+          explore the project section of my protfolio.
+        </p>
+        <p>
+          <span className="boldText">
+            What I work with: <br /> Languages:
+          </span>{" "}
+          HTML, CSS, JavaScript, TypeScript, Node.js (Open to learn more as it
+          is required by the job)
+        </p>
+        <p>
+          <span className="boldText">Technologies:</span> React.js, React
+          Native, Styled-Components, TailwindCSS, Git, Jest and React Testing
+          Tool (Open to learn more as it is required by the job)
+        </p>
+      </div>
     </div>
   );
 }
