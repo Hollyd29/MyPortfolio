94fc2b62a5d1cd4573b3ae10b0b8c3ca97975d87
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-07-22 20:14:20 +0100

    my portfolio completed

--- src/component/ContactMecomp.tsx
+++ src/component/ContactMecomp.tsx
@@ -2,7 +2,7 @@ function ContactMeComp() {
   return (
     <div className="contactCon">
       <h1 className="contactHeading">Reach Me Here!</h1>
-      <p style={{ marginBottom: 50 }}>
+      <p className="cHeadP">
         I am currently open to joining a new team of creative and curious
         developers, reach me below. Also, if you have proposals, questions or
         you just want to say 'hi', you can reach out too
